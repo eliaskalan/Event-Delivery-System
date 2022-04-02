d79c07712e47df54a81047954c8dcccd9ceb8d4a
ioannis Apostolou <giannistolou@protonmail.com>
2022-04-03 01:25:52 +0300

    Remove from commnects brokers

--- src/controller/NodeInterface.java
+++ src/controller/NodeInterface.java
@@ -4,14 +4,10 @@ import java.util.List;
 
 public interface NodeInterface {
     List<BrokerInterface> BROKER_INTERFACES =null;
-
+    List<BrokerInterface> brokers = null;
     void connect();
     void disconnect();
     //TODO add proper name
     void init(int number);
     void updateNodes();
-
-    //@TODO This must be  properties i don't know what that is
-    //List<Broker> brokers;
-
 }
