df921f40e3830457d0b929c104eb46b2c4a5ab03
Elias <eliaskalan@yahoo.gr>
2022-04-15 21:06:48 +0300

    fileName and Config
    fileName is the name of the splitted and re joint file. must be given as an input
    
    Config has the location of the videos

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,13 @@ import model.MultimediaFile;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
-        MultimediaFile.SplitVideoFile A = new MultimediaFile.SplitVideoFile();
-        A.SplitFile();
-        //MultimediaFile.JoinVideoFile B = new MultimediaFile.JoinVideoFile();
-        //B.JoinVideo();
+
+//        System.out.println("Hello World");
+//        MultimediaFile A = new MultimediaFile();
+//        A.SplitFile("Despicable Me 2 - Trailer (HD) - YouTube");
+//        MultimediaFile B = new MultimediaFile();
+//        //B.MultimediaFile();
+//        B.JoinVideo("Despicable Me 2 - Trailer (HD) - YouTube");
     }
 
 }
---
+++ src/model/Config.java
@@ -0,0 +1,5 @@
+package model;
+
+public class Config {
+    public static String SAVED_FILES_PATH= "C:/Users/elias/Documents/";
+}
