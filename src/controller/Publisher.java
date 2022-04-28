66fab0e441ef232b288dec878a0bcd4cba01e697
Elias <eliaskalan@yahoo.gr>
2022-04-28 21:28:02 +0300

    File is been send but not fully

--- src/controller/Publisher.java
+++ src/controller/Publisher.java
@@ -81,34 +81,34 @@ public class Publisher{
                     closeEverything(socket, bufferedWriter);
                 }
             }
-    public final static int SOCKET_PORT = 13267;  // you may change this
-    public final static String FILE_TO_SEND = "C:\\Users\\elias\\Documents\\image.jpeg";
+    public final static String FILE_TO_SEND = "C:\\Users\\elias\\Documents\\new.jpg";
     public void sendImage() throws IOException, InterruptedException {
         FileInputStream fis = null;
         BufferedInputStream bis = null;
         OutputStream os = null;
 
-
-
-
+        while (true) {
             System.out.println("Waiting...");
+            try {
+                System.out.println("Accepted connection : " + socket);
+                File myFile = new File(FILE_TO_SEND);
+                byte[] mybytearray = new byte[(int) myFile.length()];
+                fis = new FileInputStream(myFile);
+                bis = new BufferedInputStream(fis);
+                bis.read(mybytearray, 0, mybytearray.length);
+                os = socket.getOutputStream();
+                System.out.println("Sending " + FILE_TO_SEND + "(" + mybytearray.length + " bytes)");
+                os.write(mybytearray, 0, mybytearray.length);
+                os.flush();
+                System.out.println("Done.");
+            } finally {
+                if (bis != null) bis.close();
+                if (os != null) os.close();
+                if (socket != null) socket.close();
+            }
 
 
-            System.out.println("Accepted connection : " + socket);
-            // send file
-            File myFile = new File (FILE_TO_SEND);
-            byte [] mybytearray  = new byte [(int)myFile.length()];
-            fis = new FileInputStream(myFile);
-            bis = new BufferedInputStream(fis);
-            bis.read(mybytearray,0,mybytearray.length);
-            os = socket.getOutputStream();
-            System.out.println("Sending " + FILE_TO_SEND + "(" + mybytearray.length + " bytes)");
-            os.write(mybytearray,0,mybytearray.length);
-            os.flush();
-            System.out.println("Done.");
-
-
-
+        }
 
     }
-    }
+}
