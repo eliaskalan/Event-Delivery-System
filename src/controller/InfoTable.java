39b92bb9b4f97f1c82f2b0d5636ed2e824294296
ioannis Apostolou <giannistolou@protonmail.com>
2022-05-05 02:01:54 +0300

    create more functions

--- src/controller/InfoTable.java
+++ src/controller/InfoTable.java
@@ -10,21 +10,16 @@ import java.util.HashMap;
 
 // Important https://docs.oracle.com/javase/tutorial/essential/concurrency/syncmeth.html
 public class InfoTable {
-    private HashMap<Address, ArrayList<String>> topicsAssociatedWithBrokers = new HashMap<>();
-    private HashMap<Address, BigInteger> hashingIDAssociatedWithBrokers = new HashMap<>();
+
+    private ArrayList<Broker> availableBrokers = new ArrayList<Broker>();
+
+    private HashMap<Broker, ArrayList<Topic>> brokersConnectionWithTopics = new HashMap<>();
     private HashMap<ProfileName, ArrayList<Topic>> availableClients = new HashMap<>();
     private ArrayList<Topic> availableTopics = new ArrayList<>();
 
-    public synchronized HashMap<Address, ArrayList<String>> getTopicsAssociatedWithBrokers() {
-        return topicsAssociatedWithBrokers;
-    }
 
     public synchronized ArrayList<Topic> getAvailableTopics() {
         return availableTopics;
-    }
-
-    public synchronized HashMap<Address, BigInteger> getHashingIDAssociatedWithBrokers() {
-        return hashingIDAssociatedWithBrokers;
     }
 
     public synchronized HashMap<ProfileName, ArrayList<Topic>> getAvailableClients() {
@@ -43,6 +38,20 @@ public class InfoTable {
 
     public void availableTopics(Topic topic) {
         this.availableTopics.add(topic);
+    }
+
+    public void addBroker(Broker broker){
+        this.availableBrokers.add(broker);
+        this.brokersConnectionWithTopics.put(broker, null);
+    }
+    public void addTopicOnBroker(Broker broker, Topic topic){
+        ArrayList<Topic> topics = this.brokersConnectionWithTopics.get(broker);
+        this.brokersConnectionWithTopics.put(broker, topics);
+    }
+
+    public void addTopics(Topic topic){
+        this.availableTopics.add(topic);
+        //ToDo hashing and add in an broker
     }
 
 }
