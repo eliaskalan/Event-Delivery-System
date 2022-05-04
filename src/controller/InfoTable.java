3760777b59de851ad5b3af3b6f8432c8d5707d62
ioannis Apostolou <giannistolou@protonmail.com>
2022-05-05 01:42:24 +0300

    Create addAvailableClients and availableTopics

--- src/controller/InfoTable.java
+++ src/controller/InfoTable.java
@@ -1,6 +1,9 @@
 package controller;
 
+import model.ProfileName;
+
 import java.io.File;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,14 +12,14 @@ import java.util.HashMap;
 public class InfoTable {
     private HashMap<Address, ArrayList<String>> topicsAssociatedWithBrokers = new HashMap<>();
     private HashMap<Address, BigInteger> hashingIDAssociatedWithBrokers = new HashMap<>();
-    private HashMap<Client, ArrayList<String>> availableClients = new HashMap<>();
-    private ArrayList<String> availableTopics = new ArrayList<>();
+    private HashMap<ProfileName, ArrayList<Topic>> availableClients = new HashMap<>();
+    private ArrayList<Topic> availableTopics = new ArrayList<>();
 
     public synchronized HashMap<Address, ArrayList<String>> getTopicsAssociatedWithBrokers() {
         return topicsAssociatedWithBrokers;
     }
 
-    public synchronized ArrayList<String> getAvailableTopics() {
+    public synchronized ArrayList<Topic> getAvailableTopics() {
         return availableTopics;
     }
 
@@ -24,8 +27,22 @@ public class InfoTable {
         return hashingIDAssociatedWithBrokers;
     }
 
-    public synchronized HashMap<Client, ArrayList<String>> getAvailableClients() {
+    public synchronized HashMap<ProfileName, ArrayList<Topic>> getAvailableClients() {
         return availableClients;
+    }
+
+    public void addAvailableClients(ProfileName profile) {
+        availableClients.put(profile, new ArrayList<Topic>());
+    }
+
+    public void addAvailableClients(ProfileName profile, Topic topic) {
+        ArrayList<Topic> topics = this.availableClients.get(profile);
+        topics.add(topic);
+        availableClients.put(profile, topics);
+    }
+
+    public void availableTopics(Topic topic) {
+        this.availableTopics.add(topic);
     }
 
 }
